f3b71da3fcbd296e479ccd15142ae594b504f59d
upskubra <yildirimk268@gmail.com>
2021-08-08 13:19:16 +0300

    sign up with firebase

--- app/src/main/java/com/example/chatapp/LoginScreenFragment.kt
+++ app/src/main/java/com/example/chatapp/LoginScreenFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.chatapp.databinding.FragmentLoginScreenBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginScreenFragment : Fragment() {
@@ -13,11 +18,14 @@ class LoginScreenFragment : Fragment() {
     private var _binding: FragmentLoginScreenBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        auth = Firebase.auth
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,14 +40,38 @@ class LoginScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.TextEmailAddress.setOnClickListener { }
 
-        binding.TextPassword.setOnClickListener { }
 
-        binding.logInButton.setOnClickListener { }
 
-        binding.signInButton.setOnClickListener { }
+        binding.logInButton.setOnClickListener {
+
+
+        }
+
+        binding.signInButton.setOnClickListener {
+
+            auth.createUserWithEmailAndPassword(binding.TextEmailAddress.text.toString(), binding.TextPassword.text.toString())
+                .addOnSuccessListener {
+
+                    val action = LoginScreenFragmentDirections.actionLoginScreenFragmentToChatScreenFragment()
+                    findNavController().navigate(action)
+
+
+                }.addOnFailureListener { Exception ->
+
+
+                    println(binding.TextEmailAddress.toString())
+                    context?.let {
+
+                        Toast.makeText(it.applicationContext,Exception.localizedMessage, Toast.LENGTH_LONG).show()
+                    }
+
+
+                }
+
+        }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
